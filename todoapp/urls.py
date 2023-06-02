07278eda99227496bb96f8e7c04deda3701c5804
neerajpandit <pandeyneeraj1507@gmail.com>
2023-06-02 09:23:02 +0530

    Initialggf

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import TodoItemListView, TodoItemDetailView
 
 urlpatterns = [
-    path('todos/', TodoItemListView.as_view(), name='todo-list'),
+    path('', TodoItemListView.as_view(), name='todo-list'),
     path('todos/<int:pk>/', TodoItemDetailView.as_view(), name='todo-detail'),
     # path('token/', TokenObtainView.as_view(), name='token-obtain'),
 ]
