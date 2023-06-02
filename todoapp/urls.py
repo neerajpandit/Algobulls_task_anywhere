912ca003b8f6826945762116572a4e857be59e63
neerajpandit <pandeyneeraj1507@gmail.com>
2023-06-02 10:02:32 +0530

    Initihgflkj

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .views import TodoItemListView, TodoItemDetailView
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import TodoItemListView, TodoItemDetailView, ATodoItemListView
+
+router=DefaultRouter()
+router.register('api',ATodoItemListView)
 
 urlpatterns = [
-    path('', TodoItemListView.as_view(), name='todo-list'),
+    path('', include(router.urls)),
+    path('todo/', TodoItemListView.as_view(), name='todo-list'),
     path('todos/<int:pk>/', TodoItemDetailView.as_view(), name='todo-detail'),
     # path('token/', TokenObtainView.as_view(), name='token-obtain'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics
 from .models import TodoItem
 from .serializers import TodoItemSerializer
 from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.authentication import  BasicAuthentication
@@ -23,7 +23,11 @@ class TodoItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
-
+class ATodoItemListView(viewsets.ModelViewSet):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
 
 
 
